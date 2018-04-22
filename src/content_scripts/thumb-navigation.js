17772df589bb07ec322f11373ccb753fb715b6c7
Adam Velebil <pardalek@gmail.com>
2018-04-22 23:24:10 +0200

    Added lint configs, updated sources

--- src/content_scripts/thumb-navigation.js
+++ src/content_scripts/thumb-navigation.js
@@ -1,50 +1,51 @@
+// -*- js-indent-level: 2; -*-
 (function() {
-    if (window.hasRun) {
-        return;
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+
+  function showThumbNavigation() {
+    hideThumbNavigation();
+
+    const thumbNavigationImage = document.createElement("img");
+    thumbNavigationImage.style.display = "block";
+    thumbNavigationImage.setAttribute(
+      "src",
+      browser.extension.getURL("images/arrow_rtl.png"));
+
+    const thumbNavigation = document.createElement("div");
+    thumbNavigation.style.padding = "0px";
+    thumbNavigation.style.position = "fixed";
+    thumbNavigation.style.height = "auto";
+    thumbNavigation.style.top = "2em";
+    thumbNavigation.style.right = "2em";
+    thumbNavigation.style.background = "#ff0000";
+    thumbNavigation.style.zIndex = "100000";
+    thumbNavigation.className = "thumb-navigation";
+    thumbNavigation.style.cursor = "pointer";
+    thumbNavigation.onclick = function() {
+      window.history.back();
+    };
+
+    thumbNavigation.appendChild(thumbNavigationImage);
+    document.body.appendChild(thumbNavigation);
+  }
+
+  function hideThumbNavigation() {
+    const existingElements = document.querySelectorAll(".thumb-navigation");
+    for (const element of existingElements) {
+      element.remove();
     }
-    window.hasRun = true;
+  }
 
-    function showThumbNavigation() {
-        hideThumbNavigation();
-
-        let thumbNavigationImage = document.createElement("img");
-        thumbNavigationImage.style.display = "block";
-        thumbNavigationImage.setAttribute(
-            "src",
-            browser.extension.getURL("images/arrow_rtl.png"));
-
-        let thumbNavigation = document.createElement("div");
-        thumbNavigation.style.padding = "0px";
-        thumbNavigation.style.position = "fixed";
-        thumbNavigation.style.height = "auto";
-        thumbNavigation.style.top = "2em";
-        thumbNavigation.style.right = "2em";
-        thumbNavigation.style.background = "#aaaaffaa";
-        thumbNavigation.style.zIndex = "100000";
-        thumbNavigation.className = "thumb-navigation";
-        thumbNavigation.style.cursor = "pointer";
-        thumbNavigation.onclick = function() {
-            window.history.back();
-        };
-
-        thumbNavigation.appendChild(thumbNavigationImage);
-        document.body.appendChild(thumbNavigation);
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "show_thumb_navigation") {
+      showThumbNavigation();
+    } else if (message.command === "hide_thumb_navigation") {
+      hideThumbNavigation();
     }
+  });
 
-    function hideThumbNavigation() {
-        let existingElements = document.querySelectorAll(".thumb-navigation");
-        for (let element of existingElements) {
-            element.remove();
-        }
-    }
-
-    browser.runtime.onMessage.addListener((message) => {
-        if (message.command === "show_thumb_navigation") {
-            showThumbNavigation();
-        } else if (message.command === "hide_thumb_navigation") {
-            hideThumbNavigation();
-        }
-    });
-
-    showThumbNavigation();
+  showThumbNavigation();
 })();
